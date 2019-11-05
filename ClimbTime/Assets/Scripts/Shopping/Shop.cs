45e7e61b1c7d5e30fe994a3b61f6cc5f979e3c83
typhoonhook <50159292+Michaeljsg05@users.noreply.github.com>
2019-11-05 20:49:21 +1100

    Starting Shopping system
    Added shopping script.
    Added Triggers.
    Added input detection.

---
+++ ClimbTime/Assets/Scripts/Shopping/Shop.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    [Header("Required Settings")]
+    public Animator anim;
+    private bool inShop = false;
+
+    [Header("Shop Settings")]
+    public int itemPrice;
+    [Tooltip("Mainly for Potions and Armour")]
+    public int itemStock;
+    public Input buyKey;
+
+    [Header("Shop Input Settings")]
+    public KeyCode inputKey;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (inShop == true)
+        {
+            if (Input.GetKeyDown(inputKey))
+            {
+                Debug.Log("Buy item");
+                
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            inShop = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            inShop = false;
+        }
+    }
+
+}
