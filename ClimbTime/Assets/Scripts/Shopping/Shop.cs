a555cfee466e0760c7efbf9d3d7068cc3175bfa6
typhoonhook <50159292+Michaeljsg05@users.noreply.github.com>
2019-11-05 21:39:58 +1100

    Adding UI
    ^^ title

--- ClimbTime/Assets/Scripts/Shopping/Shop.cs
+++ ClimbTime/Assets/Scripts/Shopping/Shop.cs
@@ -6,35 +6,53 @@ public class Shop : MonoBehaviour
 {
     [Header("Required Settings")]
     public Animator anim;
+    [Tooltip("Player Requires a score real score script. When this is added i will implement that into this.")]
+    public GameObject Player;
+    public GameObject shopManager;
+    [Tooltip("Currently checking what input to look for.    ")]
     private bool inShop = false;
+    
 
     [Header("Shop Settings")]
     public int itemPrice;
     [Tooltip("Mainly for Potions and Armour")]
     public int itemStock;
-    public Input buyKey;
-
-    [Header("Shop Input Settings")]
-    public KeyCode inputKey;
+    public bool canBuy;
+    private KeyCode buyKey;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        buyKey = shopManager.gameObject.GetComponent<ShopManager>().buyKey;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (inShop == true)
+        if (Player.GetComponent<PlaceHolderScoreCounter>().score >= itemPrice && itemStock >= 1)
         {
-            if (Input.GetKeyDown(inputKey))
+            canBuy = true;
+        }
+        else if (Player.GetComponent<PlaceHolderScoreCounter>().score >= itemPrice)
+        {
+            canBuy = false;
+        }
+        else if (itemStock < 1)
+        {
+            canBuy = false;
+        }
+
+
+
+        if (inShop == true && canBuy == true)
+        {
+            if (Input.GetKeyDown(buyKey))
             {
-                Debug.Log("Buy item");
-                
+                Debug.Log("Buy");
+                Player.GetComponent<PlaceHolderScoreCounter>().score -= itemPrice;
+                itemStock--;
             }
         }
     }
