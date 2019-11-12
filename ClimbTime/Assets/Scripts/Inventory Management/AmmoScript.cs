d1a3478b3939ac5f03fec04e13e7ff29c78a602d
typhoonhook <50159292+Michaeljsg05@users.noreply.github.com>
2019-11-12 17:42:20 +1100

    ammo.

---
+++ ClimbTime/Assets/Scripts/Inventory Management/AmmoScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoScript : MonoBehaviour
+{
+
+    public KeyCode fireButton;
+    public int ammo;
+    public float cooldownTime;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(fireButton) && ammo <= 1)
+        {
+            Shoot();
+        }
+
+    }
+
+    public void Shoot()
+    {
+        ammo--;
+    }
+    
+
+}
+
