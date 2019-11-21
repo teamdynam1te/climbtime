2ebda250370d02094fb5fc17f514a8205a117201
typhoonhook <50159292+Michaeljsg05@users.noreply.github.com>
2019-11-21 20:35:51 +1100

    rockEnhancement

---
+++ ClimbTime/Assets/Scripts/Other/SlowRotateRock.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowRotateRock : MonoBehaviour
+{
+
+    public float speed;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = this.GetComponentInParent<RockFallScript>().moveX;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        this.transform.Rotate(0, 0, speed * speed * 5000 * Time.deltaTime);
+
+
+    }
+}
